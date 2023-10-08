6961afa6161699f9f50855985e14c97461cea42d
LuigiCerone <luigicerone96@gmail.com>
2023-10-08 20:37:01 +0200

    First steps of the tutorial

---
+++ models/staging/jaffle_shop/base/base_jaffle_shop__deleted_customers.sql
@@ -0,0 +1,19 @@
+with
+
+source as (
+
+    select * from {{ source('jaffle_shop','customer_deletes') }}
+
+),
+
+deleted_customers as (
+
+    select
+        id as customer_id,
+        deleted as deleted_at
+
+    from source
+
+)
+
+select * from deleted_customers
