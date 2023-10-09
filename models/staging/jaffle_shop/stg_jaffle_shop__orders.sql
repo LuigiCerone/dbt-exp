2b88edac13ad4ef4c1956be56032c628f965e172
LuigiCerone <luigicerone96@gmail.com>
2023-10-09 22:23:15 +0200

    File refactoring according to official doc

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,25 @@
+with
+
+source as (
+
+    select * from {{ source('ecom', 'raw_orders') }}
+    where ordered_at <= {{ var('truncate_timespan_to') }}
+
+),
+
+renamed as (
+
+    select
+
+        id as order_id,
+        store_id as location_id,
+        customer as customer_id,
+        (order_total / 100.0) as order_total,
+        (tax_paid / 100.0) as tax_paid,
+        {{ dbt.date_trunc('day','ordered_at') }} as ordered_at
+
+    from source
+
+)
+
+select * from renamed
